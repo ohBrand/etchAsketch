d42ff6c2654ebff2f9e44494c396b66276bf3b37
Brandon <brandoncep96@mail.com>
2024-11-07 12:17:23 -0500

    added functionality to custom Grid button

--- stuff.js
+++ stuff.js
@@ -2,38 +2,53 @@ const container = document.getElementById('container')
 const eraser = document.querySelector('.erase-el')
 const newGrid = document.querySelector('.gridcreator')
 let board;
+let gridNumber;
 let boardArray = []
 let gridSize;
+
+function getRandomcolor() {
+    let letters = '0123456789ABCDEF'
+    let color = '#'
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)]
+    }
+    return color
+}
+
 
 function createNewGrid() {
     container.innerHTML = ''
 
-    gridSize = parseInt(prompt('enter a number for a new grid size(max 100x100): '))
+    gridNumber = parseInt(prompt('enter a number for a new grid size(max 100x100): '))
 
-    gridSize = gridSize * gridSize
+    if (isNaN(gridNumber) || gridNumber > 100) {
+        gridNumber = parseInt(prompt('Numebr is either too large or not a number at all enter an number thats 100 or less: '))
+    }
+
+    renderGrid(gridNumber)
+
 }
 
 
 
+function renderGrid(grid) {
 
-function renderGrid(grid = 256) {
+    const squareSize = 600 / grid
 
-    for (let i = 0; i < grid; i++) {
+    for (let i = 0; i < grid * grid; i++) {
         board = document.createElement('div')
         board.classList.add('square')
+        board.style.width = `${squareSize}px`
+        board.style.height = `${squareSize}px`
         container.appendChild(board)
         boardArray.push(board)
     }
 }
 
-renderGrid()
+renderGrid(gridNumber = 16)
 
 container.addEventListener('mouseover', (event) => {
-    event.target.style.backgroundColor = '#FAB30C'
-
-    // setTimeout(() => {
-    //     event.target.style.backgroundColor = 'slategray'
-    // }, 1500)
+    event.target.style.backgroundColor = getRandomcolor()
     
     container.style.backgroundColor = 'white'
 })
